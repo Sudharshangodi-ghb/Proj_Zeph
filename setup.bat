226129c91db209fb1267a3024ce480088438ca28
Sudharshan Reddy Godi <33070170+Sudharshangodi-ghb@users.noreply.github.com>
2025-12-21 14:19:21 +0530

    Update setup.bat to remove comments and enhance commands

--- setup.bat
+++ setup.bat
@@ -1,13 +1,15 @@
-REM winget install Kitware.CMake Ninja-build.Ninja oss-winget.gperf Python.Python.3.12 Git.Git oss-winget.dtc wget 7zip.7zip
 
-REM @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command " [System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+winget install Kitware.CMake Ninja-build.Ninja oss-winget.gperf Python.Python.3.12 Git.Git oss-winget.dtc wget 7zip.7zip
 
-REM choco -v
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command " [System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-REM choco install vscode
+choco -v
+
+choco install vscode
+
 python -m venv py_env
 
-call py_env\Scripts\activate.bat
+call py_env\Scripts\Activate.ps1
 
 python.exe -m pip install --upgrade pip
 
@@ -23,4 +25,4 @@ west update
 
 wesr zephyr-export 
 
-west sdk install
+west sdk install "."
